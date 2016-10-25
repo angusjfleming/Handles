4299ef8e1dc6f6cf1e14aee0916808839623a7d1
gus <gusmagician@gmail.com>
2016-10-25 11:27:49 +0100

    Added serverinfo command

---
+++ cmd/serverinfo.js
@@ -0,0 +1,9 @@
+exports.run = (bot, msg, params = []) => {
+  msg.channel.sendMessage('```xl\nName: ' + msg.guild.name + '\nIcon Url: ' + msg.guild.iconURL + '\nRegion: ' + msg.guild.region + '\nOwner: ' + msg.guild.owner.user.username + "#" + msg.guild.owner.user.discriminator +' (' + msg.guild.owner.user.id + ')\nCreation Date: '+ msg.guild.creationDate +'```');
+};
+
+exports.help = {
+  name : "serverinfo",
+  description: "Returns misc server info",
+  usage: "serverinfo"
+};
