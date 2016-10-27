b2df8372b670216cb052468cea209c674df242d1
Gus <gusmagician@gmail.com>
2016-10-27 11:07:42 +0100

    Added cleanjs.js beautified code.
    With thanks to http://jsbeautifier.org/

--- cmd/serverinfo.js
+++ cmd/serverinfo.js
@@ -1,9 +1,9 @@
 exports.run = (bot, msg, params = []) => {
-  msg.channel.sendMessage('```xl\nName: ' + msg.guild.name + '\nIcon Url: ' + msg.guild.iconURL + '\nRegion: ' + msg.guild.region + '\nOwner: ' + msg.guild.owner.user.username + "#" + msg.guild.owner.user.discriminator +' (' + msg.guild.owner.user.id + ')\nCreation Date: '+ msg.guild.createdAt +'```');
+    msg.channel.sendMessage('```xl\nName: ' + msg.guild.name + '\nIcon Url: ' + msg.guild.iconURL + '\nRegion: ' + msg.guild.region + '\nOwner: ' + msg.guild.owner.user.username + "#" + msg.guild.owner.user.discriminator + ' (' + msg.guild.owner.user.id + ')\nCreation Date: ' + msg.guild.createdAt + '```');
 };
 
 exports.help = {
-  name : "serverinfo",
-  description: "Returns misc server info",
-  usage: "serverinfo"
+    name: "serverinfo",
+    description: "Returns misc server info",
+    usage: "serverinfo"
 };
