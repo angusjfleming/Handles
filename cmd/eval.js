fe9f0e310fd7bbb96f3fc5b44cf0289ca5b2bdef
Gus <gusmagician@gmail.com>
2016-10-28 16:10:50 +0100

    Updated eval.js
    Remove obsolete code

--- cmd/eval.js
+++ cmd/eval.js
@@ -1,36 +1,4 @@
-const util = require('util');
-var fs = require("fs");
-
-function clean(text) {
-    if (typeof(text) === "string") {
-        return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    } else {
-        return text;
-    }
-}
-
 exports.run = (bot, msg, params = []) => {
-    /*console.log(util.inspect(msg.member))*/
-    /*console.log(permlvl)
-    var code = params.join(" ");
-    if (permlvl = 0) {msg.channel.sendMessage("`FAILED` ```xl\nThis command is reserved for the bot owner. \n```")} return;
-    try {
-        var evaled = eval(code);
-        if (typeof evaled !== 'string')
-            evaled = require('util').inspect(evaled);
-        	msg.channel.sendMessage("`SUCCESS` ```xl\n" + evaled + "\n```");
-        if (evaled.length > 2000) {
-            msg.channel.sendMessage("Trying to eval: ` " + code + " ` failed because the output was greater than 2000 characters")
-        }
-        return;
-        msg.channel.sendMessage("`SUCCESS` ```xl\n" + clean(evaled) + "\n```");
-        msg.channel.sendMessage("`SUCCESS` ```xl\n" + clean(evaled) + "\n```");
-    } catch (err) {
-        msg.channel.sendMessage("`FAILED` ```xl\n" +
-            clean(err) +
-            "\n```");
-    }*/
-
     var code = params.join(" ");
     try {
         if (eval(code) > 2000) {
