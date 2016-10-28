a8cbd4fe8da05945a5bade989fcae9d674f7735d
Gus <gusmagician@gmail.com>
2016-10-28 16:15:49 +0100

    Updated shardmanage.js
    apparently _spawn works better

--- shardmanage.js
+++ shardmanage.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 const manager = new Discord.ShardingManager('bot.js');
-manager.spawn(2);
+manager._spawn(2);
 manager.on('launch', shard => {
   console.log(`Launched shard ${shard.id}`);
 });
