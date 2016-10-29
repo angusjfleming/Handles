2241ac0bc69e19fbd4c8eafb9815715bd897cb29
Gus <gusmagician@gmail.com>
2016-10-29 01:53:54 +0100

    Added bot.reload
    yes

--- bot.js
+++ bot.js
@@ -90,3 +90,24 @@ bot.elevation = function(msg) {
   if(msg.author.id === ownerid) permlvl = 3;
   return permlvl;
 };
+
+bot.reload = function(command) {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./cmd/${command}`)];
+      let cmd = require(`./cmd/${command}`);
+      bot.commands.delete(command);
+      bot.aliases.forEach((cmd, alias) => {
+        if (cmd === command) bot.aliases.delete(alias);
+      });
+
+      bot.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        bot.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
