2f35b44bbf5f99425b6d592d1ba59121f46bfe6b
Gus <gusmagician@gmail.com>
2016-10-30 22:52:16 +0000

    added invite.js
    sends bot invite link, needs to get bot client id but this will do for
    now

---
+++ cmd/invite.js
@@ -0,0 +1,15 @@
+exports.run = (bot, msg, params = []) => {
+	msg.channel.sendMessage("https://discordapp.com/oauth2/authorize?&client_id=168395434371448832&scope=bot&permissions=8");
+};
+
+exports.help = {
+    name: "invite",
+    description: "Returns bot invite",
+    usage: "invite"
+};
+
+exports.conf = {
+  enabled: true,
+  aliases: ['inv'],
+  permLevel: 0
+};
