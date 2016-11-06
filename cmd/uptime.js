618dde2040732ea4d15df4ea5c2c8b2e03e33c7e
Gus <gusmagician@gmail.com>
2016-11-06 12:30:50 +0000

    Added uptime.js
    rather self explanatory

---
+++ cmd/uptime.js
@@ -0,0 +1,27 @@
+function msToTime(s) {
+  var ms = s % 1000;
+  s = (s - ms) / 1000;
+  var secs = s % 60;
+  s = (s - secs) / 60;
+  var mins = s % 60;
+  var hrs = (s - mins) / 60;
+
+  uptimehms = (hrs + ':' + mins + ':' + secs);
+}
+
+exports.run = (bot, msg, params = []) => {
+	msgToTime(bot.uptime)
+	msg.channel.sendMessage(uptimehms)
+};
+
+exports.help = {
+    name: "uptime",
+    description: "Returns bot uptime",
+    usage: "uptime"
+};
+
+exports.conf = {
+  enabled: true,
+  aliases: [],
+  permLevel: 2
+};
