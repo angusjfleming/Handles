f56a2ef69109411903830e97e63aa4b9127408cc
Gus <gusmagician@gmail.com>
2016-11-12 13:33:42 +0000

    Updated remindme
    Now more passive aggressive and beautiful

--- cmd/remindme.js
+++ cmd/remindme.js
@@ -1,18 +1,24 @@
 exports.run = (bot, msg, params = []) => {
-  var minno = params[0]
-  mins = parseInt(minno) * 60 * 1000;
-  if (isNaN(mins)){
-      msg.channel.sendMessage(`${params[0]} is not a valid quantity of time.`)
-      return;
-  }
-  params.shift();
-  var reminder = params.join(" ")
-  msg.channel.sendMessage(`Reminder for \`${reminder}\` set for ${minno} minute(s).`)
-  setTimeout(function(){ reply(msg, reminder); }, mins);
+    var minno = params[0]
+        (mins) = parseInt(minno) * 60 * 1000;
+    if (mins = "undefined") {
+        msg.channel.sendMessage("You did not enter any parameters. **At all**.")
+        return;
+    }
+    if (isNaN(mins)) {
+        msg.channel.sendMessage(`${params[0]} is not a valid quantity of time.`)
+        return;
+    }
+    params.shift();
+    var reminder = params.join(" ")
+    msg.channel.sendMessage(`Reminder for \`${reminder}\` set for ${minno} minute(s).`)
+    setTimeout(function() {
+        reply(msg, reminder);
+    }, mins);
 };
 
 function reply(msg, reminder) {
-  return msg.reply(` ${reminder}`)
+    return msg.reply(` ${reminder}`)
 };
 
 exports.help = {
@@ -22,7 +28,7 @@ exports.help = {
 };
 
 exports.conf = {
-  enabled: true,
-  aliases: ['remind'],
-  permLevel: 0
+    enabled: true,
+    aliases: ['remind'],
+    permLevel: 0
 };
