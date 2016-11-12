c513d158b97494ecadcdf1169300ed56ded67055
Gus <gusmagician@gmail.com>
2016-11-12 13:58:47 +0000

    Updated exec.js
    Better logging of events

--- cmd/exec.js
+++ cmd/exec.js
@@ -1,13 +1,17 @@
 var exec = require('child_process').exec;
 exports.run = (bot, msg, params = []) => {
   var code = params.join(" ")
-  exec(code, function(error, stdout, stderr) {
-      msg.channel.sendMessage('stdout: ', stdout);
-      msg.channel.sendMessage('stderr: ', stderr);
-      if (error !== null) {
-          msg.channel.sendMessage('exec error: ', error);
-      }
+  var child = exec(code);
+  child.stdout.on('data', function(data) {
+      msg.channel.sendMessage('stdout: ' + data);
   });
+  child.stderr.on('data', function(data) {
+      msg.channel.sendMessage('stdout: ' + data);
+  });
+  child.on('close', function(code) {
+      msg.channel.sendMessage('closing code: ' + code);
+  });
+
 };
 
 exports.help = {
