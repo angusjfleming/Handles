68d314de7162ffcb229c43fac2764016012b3845
Gus <gusmagician@gmail.com>
2016-11-19 20:56:13 +0000

    Updated rebootfast.js
    never used, but still needs updating /shrug

--- cmd/rebootfast.js
+++ cmd/rebootfast.js
@@ -1,20 +1,17 @@
 exports.run = (bot, msg) => {
-            msg.channel.sendMessage(`Rebooting...`).then(() => {
-                bot.user.setGame(`Rebooting...`)
-                process.exit();
-            }).catch(e => {
-                console.error(e);
-            });
-        }
+  msg.channel.sendMessage(`Rebooting...`)
+  bot.user.setGame(`Rebooting... **quickly**`)
+  process.exit();
+}
 
 exports.help = {
-    name: `rebootfast`,
-    description: `Reboots the bot. **fast**`,
-    usage: `rebootfast`
+  name: `rebootfast`,
+  description: `Reboots the bot. **fast**`,
+  usage: `rebootfast`
 };
 
 exports.conf = {
-    enabled: true,
-    aliases: ['fastrestart', 'rebootfast', 'fast'],
-    permLevel: 3
+  enabled: true,
+  aliases: ['fastrestart', 'rebootfast', 'fast'],
+  permLevel: 3
 };
