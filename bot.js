23a6a9aaca98d63815a0950751f45d56dd2eacc2
Gus <gusmagician@gmail.com>
2016-11-20 13:06:24 +0000

    Updated bot.js
    now the guild owner doesn't need the Admin Role

--- bot.js
+++ bot.js
@@ -94,7 +94,7 @@ process.on("unhandledRejection", err => {
 bot.elevation = function(msg) {
     let permlvl = 0;
     let admin_role = msg.guild.roles.find("name", "Admin");
-    if (admin_role && msg.member.roles.has(admin_role.id))
+    if (admin_role && msg.member.roles.has(admin_role.id) || msg.author.id == msg.guild.owner.id)
         permlvl = 2;
     if (msg.author.id === ownerid)
         permlvl = 3;
