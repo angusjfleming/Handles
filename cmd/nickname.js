9b673494ceb84612b85c082c793ccfde5aa13acd
Gus <gusmagician@gmail.com>
2016-11-20 21:53:50 +0000

    Updated nickname.js
    now only tries to log if something has changed.

--- cmd/nickname.js
+++ cmd/nickname.js
@@ -1,6 +1,7 @@
 var hex = "551a8b";
 exports.run = (bot, msg, params = []) => {
         var info;
+        var changed = false;
         try {
             var nickuser = msg.mentions.users.first()
             nickuser = msg.guild.member(nickuser)
@@ -15,10 +16,12 @@ exports.run = (bot, msg, params = []) => {
                 return;
             }
             if (params.length == 0) {
+                changed = true;
                 msg.delete()
                 nickuser.setNickname(params.join(` `))
                 info = `Reset ${msg.mentions.users.first().username}#${msg.mentions.users.first().discriminator}\'s nickname.`
             } else {
+                changed = true;
                 msg.delete()
                 nickuser.setNickname(params.join(` `))
                 info = `Set ${msg.mentions.users.first().username}#${msg.mentions.users.first().discriminator}\'s nickname to ${params.join(` `)}`;
@@ -26,10 +29,9 @@ exports.run = (bot, msg, params = []) => {
   } catch (err) {
     msg.reply(`Failed with error ${err}`)
   }
-
-
+if (changed){
   bot.modlog(msg, exports.help.name, info, hex)
-
+}
 };
 
 exports.help = {
