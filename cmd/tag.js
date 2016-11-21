439d8e7bf31d90fc3fc2326f0e4c55f7e1044a89
Gus <gusmagician@gmail.com>
2016-11-21 21:07:02 +0000

    Updated tag commands to use proper directory
    added tag file

--- cmd/tag.js
+++ cmd/tag.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./selfbot.sqlite');
+const db = new sqlite3.Database('../tags.sqlite');
 
 exports.run = (bot, msg, params = []) => {
     db.serialize(function() {
