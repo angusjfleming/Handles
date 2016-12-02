388e2d4a38ad89882e7709f76e18b961142a363e
Gus <gusmagician@gmail.com>
2016-12-02 23:33:11 +0000

    Updated math.js
    beautified

--- cmd/math.js
+++ cmd/math.js
@@ -1,25 +1,25 @@
 var math = require('mathjs');
 exports.run = (bot, msg, params = []) => {
-    var code = params.join(" ");
-    try {
-        var evaled = math.eval(code);
-    } catch (err) {
-      msg.channel.sendMessage(`Sorry bro, i just couldn\'t find it in me to maths that.
+  var code = params.join(" ");
+  try {
+    var evaled = math.eval(code);
+  } catch (err) {
+    msg.channel.sendMessage(`Sorry bro, i just couldn\'t find it in me to maths that.
 ${err}`)
-    }
-    if (evaled) {
-      msg.channel.sendMessage(`\`\`\`${evaled}\`\`\``)
-    }
+  }
+  if (evaled) {
+    msg.channel.sendMessage(`\`\`\`${evaled}\`\`\``)
+  }
 };
 
 exports.help = {
-    name: `math`,
-    description: `Evaluates maths`,
-    usage: `math <maths>`
+  name: `math`,
+  description: `Evaluates maths`,
+  usage: `math <maths>`
 };
 
 exports.conf = {
-    enabled: true,
-    aliases: ['calc', 'maths'],
-    permLevel: 1
+  enabled: true,
+  aliases: ['calc', 'maths'],
+  permLevel: 1
 };
