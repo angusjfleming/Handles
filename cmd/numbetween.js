8df4fadc4457b2ac9282248ed945b8ee3cf68577
Gus <gusmagician@gmail.com>
2016-12-03 13:27:34 +0000

    Added numbetween.js
    guess what it does

---
+++ cmd/numbetween.js
@@ -0,0 +1,19 @@
+exports.run = (bot, msg, params = []) => {
+ var num1 = params[0]
+ var num2 = params[1]
+  var choice = Math.floor(Math.random() * num2);
+  if (choice < num1){choice = num1}
+  msg.channel.sendMessage(`Between ${num1} and ${num2}, I chose: \`${choice}\``)
+};
+
+exports.help = {
+    name: `numbetween`,
+    description: `Finds a number between x and y`,
+    usage: `numbetween <num1> <num2>`
+};
+
+exports.conf = {
+    enabled: true,
+    aliases: [],
+    permLevel: 1
+};
