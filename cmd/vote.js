7046c866386542c4517942dda57c0094d4c51c42
Gus <gusmagician@gmail.com>
2016-12-03 20:09:58 +0000

    Updated vote.js
    was missing a bracket

--- cmd/vote.js
+++ cmd/vote.js
@@ -22,7 +22,7 @@ exports.run = (bot, msg) => {
         if (no > yes){
           msg.channel.sendMessage(`Voting on \`${params.join(" ")}\` has ended. No won with ${no} votes. In comparison, yes only had ${yes} votes. Pitiful. :joy: `)
         }
-};
+});
 
 exports.help = {
     name: `vote`,
