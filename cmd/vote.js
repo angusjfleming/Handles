dcca973841793051b79c04689f9d66e9d471e362
Gus <gusmagician@gmail.com>
2016-12-03 20:29:28 +0000

    Updated vote.js
    more love and care fixed stuff being the wrong way round
    and now the collector has filter so yey

--- cmd/vote.js
+++ cmd/vote.js
@@ -1,25 +1,23 @@
 exports.run = (bot, msg, params = []) => {
-  msg.channel.sendMessage(`${msg.author.username} created a vote on \`${params.join(" ")}\`. Type yes or no in the chat to vote!`)
+  msg.channel.sendMessage(`${msg.author.username} created a vote on \`${params.join(" ")}\`. The vote will end in 30s, type yes or no in the chat to vote!`)
   var yes = 0;
   var no = 0;
-    const votecollector = msg.channel.createCollector({
-        time: 30000
-    });
+    const votecollector = msg.channel.createCollector(m => m.channel === msg.channel, {time: 30000});
     votecollector.on(`message`, m => {
         if (m.content === `no` && !msg.author.voted){
-            yes++;
+            no++;
             msg.author.voted = true;
           }
         if (m.content === `yes` && !msg.author.voted){
-            no++;
+            yes++;
             msg.author.voted = true;
           }
     });
     votecollector.on(`end`, (collected, reason) => {
         if (yes > no){
-          msg.channel.sendMessage(`Voting on \`${params.join(" ")}\` has ended. Yes won with ${yes} votes. No only had ${no} votes. :sad: `)
+          msg.channel.sendMessage(`Voting on \`${params.join(" ")}\` has ended. Yes won with ${yes} votes. No only had ${no} votes. :cry: `)
         }
-        if (no > yes){
+        if (yes < no){
           msg.channel.sendMessage(`Voting on \`${params.join(" ")}\` has ended. No won with ${no} votes. In comparison, yes only had ${yes} votes. Pitiful. :joy: `)
         }
 });
