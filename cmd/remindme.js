9846c584f882e808830daaaaac68295b8499a5a5
Gus <gusmagician@gmail.com>
2016-12-10 20:12:40 +0000

    Updated reminde.js
    now you need a reminder

--- cmd/remindme.js
+++ cmd/remindme.js
@@ -8,6 +8,10 @@ exports.run = (bot, msg, params = []) => {
     }
     params.shift();
     var reminder = params.join(" ")
+    if (!reminder){
+      msg.reply(`Sorry bro, that won't fly here, you gotta be reminded of something. :eyes:`).then(m => {setTimeout(m.delete.bind(m), 10000)})
+      return;
+    }
     msg.reply(`Reminder for \`${reminder}\` set for ${minno} minute(s).`).then(m => {setTimeout(m.delete.bind(m), 10000)})
     setTimeout(function() {
         reply(msg, reminder);
