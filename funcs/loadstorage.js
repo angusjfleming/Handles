db648d30e2c327c8abdb08a3f87733747044f098
Gus <gusmagician@gmail.com>
2016-12-19 14:52:55 +0000

    Updated loadstorage

--- funcs/loadstorage.js
+++ funcs/loadstorage.js
@@ -1,4 +1,4 @@
 module.exports = (bot, fs) => {
-bot.notes = JSON.parse(fs.readFileSync('./localstorage/notes.json', 'utf8'));
-bot.guildconfs = JSON.parse(fs.readFileSync('./localstorage/guildconfs.json', 'utf8'));
+bot.notes = JSON.parse(fs.readFileSync('localstorage/notes.json', 'utf8'));
+bot.guildconfs = JSON.parse(fs.readFileSync('localstorage/guildconfs.json', 'utf8'));
 };
