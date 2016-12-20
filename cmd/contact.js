6e7894063639786d80694c0fa7a9eb6311667a8b
Gus <gusmagician@gmail.com>
2016-12-20 02:08:57 +0000

    Updated contact, loadstorage
    loadstorage creates notes.json if it doesn't already exist
    contact :ok_hand:

--- cmd/contact.js
+++ cmd/contact.js
@@ -2,9 +2,15 @@ exports.run = (bot, msg, params = []) => {
     var inv;
     channel = bot.guilds.get(bot.hubchannel).defaultChannel
     msg.channel.createInvite()
-        .then(invite => channel.sendMessage(`${msg.author.username}#${msg.author.discriminator} (${msg.author.id}) said: ${params.join(" ")}\n${invite.url}`))
+        .then(invite => contact(invite, msg, channel, params))
         .catch(console.error)
 };
+
+function contact(invite, msg, channel, params){
+    channel.sendMessage(`${msg.author.username}#${msg.author.discriminator} (${msg.author.id}) said: ${params.join(" ")}\n${invite.url}`)
+    msg.delete()
+    msg.reply(`Successfully contacted owner.`).then(m => {setTimeout(m.delete.bind(m), 10000)})
+}
 
 exports.help = {
     name: "contact",
--- funcs/loadstorage.js
+++ funcs/loadstorage.js
@@ -1,4 +1,10 @@
 module.exports = (bot, fs) => {
+  if (fs.existsSync('localstorage/notes.json')){
 bot.notes = JSON.parse(fs.readFileSync('localstorage/notes.json', 'utf8'));
-bot.guildconfs = JSON.parse(fs.readFileSync('localstorage/guildconfs.json', 'utf8'));
+} else {
+  fs.writeFile('localstorage/notes.json', JSON.stringify({}), (err) => {if (err) console.log(err);});
+  setTimeout(function(){
+  bot.notes = JSON.parse(fs.readFileSync('localstorage/notes.json', 'utf8'));
+}, 500);
+}
 };
