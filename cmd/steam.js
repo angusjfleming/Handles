4c489661a86c5be515e126ca2ef147229f99fc43
Gus <gusmagician@gmail.com>
2016-12-24 17:15:52 +0000

    Removed mdn, added steam, edited package.json

---
+++ cmd/steam.js
@@ -0,0 +1,49 @@
+var request = require('request');
+var humanizeDuration = require('humanize-duration')
+exports.run = (bot, msg, params = []) => {
+  request('http://steamgaug.es/api/v2', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var info = JSON.parse(body)
+      } else {
+        msg.channel.sendMessage(`API seems to be down, try again soon.`)
+        return;
+      }
+      var empty = "";
+      message = (`\`\`\`ini
+[Steam Status]
+Steam: ${info.ISteamClient.online} ${empty}
+Steam Community: ${info.SteamCommunity.online} (${info.SteamCommunity.time}ms)
+Steam Store: ${info.SteamStore.online} (${info.SteamStore.time}ms)
+Steam User API: ${info.ISteamUser.online} (${info.ISteamUser.time}ms)
+
+[Dota 2 Status]
+API: ${info.IEconItems[570].online} (${info.IEconItems[570].time}ms)
+Game Coordinator: ${info.ISteamGameCoordinator[570].online} ${empty}
+Players Searching: ${info.ISteamGameCoordinator[570].stats.players_searching}
+
+[CS:GO Status]
+API: ${info.IEconItems[730].online} (${info.IEconItems[730].time}ms)
+Game Coordinator: ${info.ISteamGameCoordinator[730].online} ${empty}
+Players Searching: ${info.ISteamGameCoordinator[730].stats.players_searching}
+Average Wait: ${humanizeDuration(info.ISteamGameCoordinator[730].stats.average_wait)}
+
+[TF2 Status]
+API: ${info.IEconItems[440].online} (${info.IEconItems[440].time}ms)
+Game Coordinator: ${info.ISteamGameCoordinator[440].online} ${empty}
+\`\`\``)
+message = message.replace(/: 1 /g, ": Online ");
+message = message.replace(/: 2 /g, ": Offline ");
+      msg.channel.sendMessage(message)
+  })
+};
+
+exports.help = {
+    name: "steam",
+    description: "Returns status of steam.",
+    usage: "steam"
+};
+
+exports.conf = {
+    enabled: true,
+    permLevel: 1
+};
