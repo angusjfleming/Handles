27e103d33892b58890588df60c1aabbef9204848
Gus <gusmagician@gmail.com>
2017-04-29 09:30:22 +0100

    uses absoulte position because it doesn't need to be relative

--- funcs/logmessage.js
+++ funcs/logmessage.js
@@ -2,21 +2,21 @@ const fs = require('fs')
 
 module.exports = (bot, msg) => {
 
-if (!fs.existsSync(`./localstorage`)){
-    fs.mkdirSync(`./localstorage`);
+if (!fs.existsSync(`localstorage`)){
+    fs.mkdirSync(`localstorage`);
 }
 
-if (!fs.existsSync(`./localstorage/${msg.guild.id}.json`)){
+if (!fs.existsSync(`localstorage/${msg.guild.id}.json`)){
     createjson(msg.guild.id)
 }
 
 setTimeout(function() {
-var obj = require(`./localstorage/${msg.guild.id}.json`);
+var obj = require(`localstorage/${msg.guild.id}.json`);
 var dataadress = obj.length + 1
 obj[dataadress][0] = msg.author;
 obj[dataadress][1] = msg.createdAt;
 obj[dataadress][2] = msg.id;
-fs.writeFile(`./localstorage/${msg.guild.id}.json`, JSON.stringify(obj), function (err) {
+fs.writeFile(`localstorage/${msg.guild.id}.json`, JSON.stringify(obj), function (err) {
   console.log(err);
 });
 }, 500);
