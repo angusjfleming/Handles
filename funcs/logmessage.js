b93bc36bbad05e37f390dcfbf9b931bb143201c6
Gus <gusmagician@gmail.com>
2017-04-30 11:54:24 +0100

    hum

--- funcs/logmessage.js
+++ funcs/logmessage.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 
 module.exports = (bot, msg) => {
 
-if (!fs.existsSync(`../localstorage`)){
-    fs.mkdirSync(`../localstorage`);
+if (!fs.existsSync(`./localstorage`)){
+    fs.mkdirSync(`./localstorage`);
 }
 
-if (!fs.existsSync(`../localstorage/${msg.guild.id}.json`)){
-    createjson(msg.guild.id)
+if (!fs.existsSync(`./localstorage/${msg.guild.id}.json`)){
+    createjson(msg.guild.id, msg)
 }
 
 var obj = require(`../localstorage/${msg.guild.id}.json`);
@@ -17,14 +17,15 @@ msgdata[msg.id][0] = msg.author;
 msgdata[msg.id][1] = msg.createdAt;
 msgdata[msg.id][2] = msg.content;
 obj.concat(msgdata)
+console.log(obj)
 
-fs.writeFile(`../localstorage/${msg.guild.id}.json`, JSON.stringify(obj), function (err) {
+fs.writeFile(`./localstorage/${msg.guild.id}.json`, JSON.stringify(obj), function (err) {
   console.log(err);
 });
 }
 
-function createjson(jsonname) {
-      fs.writeFile(`../localstorage/${jsonname}.json`, JSON.stringify({}), (err) => {
+function createjson(jsonname, msg) {
+      fs.writeFile(`./localstorage/${jsonname}.json`, JSON.stringify({}), (err) => {
           if (err) console.log(err);
       });
 }
