9de6c01a6afe2d138ea8e64babb39afc386a9f99
Gus <gusmagician@gmail.com>
2017-04-30 11:37:13 +0100

    fixing file references I hope

--- funcs/logmessage.js
+++ funcs/logmessage.js
@@ -2,15 +2,15 @@ const fs = require('fs')
 
 module.exports = (bot, msg) => {
 
-if (!fs.existsSync(`localstorage`)){
-    fs.mkdirSync(`localstorage`);
+if (!fs.existsSync(`/localstorage`)){
+    fs.mkdirSync(`/localstorage`);
 }
 
-if (!fs.existsSync(`localstorage/${msg.guild.id}.json`)){
+if (!fs.existsSync(`/localstorage/${msg.guild.id}.json`)){
     createjson(msg.guild.id)
 }
 
-var obj = require(`localstorage/${msg.guild.id}.json`);
+var obj = require(`/localstorage/${msg.guild.id}.json`);
 console.log(obj)
 var msgdata = []
 msgdata[msg.id][0] = msg.author;
@@ -18,13 +18,13 @@ msgdata[msg.id][1] = msg.createdAt;
 msgdata[msg.id][2] = msg.content;
 obj.concat(msgdata)
 
-fs.writeFile(`localstorage/${msg.guild.id}.json`, JSON.stringify(obj), function (err) {
+fs.writeFile(`/localstorage/${msg.guild.id}.json`, JSON.stringify(obj), function (err) {
   console.log(err);
 });
 }
 
 function createjson(jsonname) {
-      fs.writeFile(`localstorage/${jsonname}.json`, JSON.stringify({}), (err) => {
+      fs.writeFile(`/localstorage/${jsonname}.json`, JSON.stringify({}), (err) => {
           if (err) console.log(err);
       });
 }
