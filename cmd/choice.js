5b649349c1d5a77dfa883eaa283a85b2f8057900
Gus <gusmagician@gmail.com>
2017-05-10 21:06:44 +0100

    last try

--- cmd/choice.js
+++ cmd/choice.js
@@ -1,11 +1,11 @@
 exports.run = (bot, msg, params = []) => {
-    if ((params = "") != false) {
-    } else {
     var choices = params.join(` `)
     var tokens = [].concat.apply([], choices.split('"').map(function(v, i) {
         return i % 2 ? v : v.split(' ')
     })).filter(Boolean);
     var choice = Math.floor(Math.random() * tokens.length);
+    if (!tokens[choice]) {
+    } else {
     msg.channel.sendMessage(`My choice was: \`${tokens[choice]}\``)
     }
 };
