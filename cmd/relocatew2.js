b54601c2fd73a6d27f8fe56eed9abae2fceed7ab
Gus <gusmagician@gmail.com>
2017-06-07 15:16:13 +0100

    TO BM W2

---
+++ cmd/relocatew2.js
@@ -0,0 +1,15 @@
+exports.run = (bot, msg, params = []) => {
+    var dubya = msg.guild.members.find('id', '83864099842195456')
+    dubya.setVoiceChannel(msg.guild.channels.filter(e => e.type === 'voice').random())
+};
+
+exports.conf = {
+    aliases: ["gtfow2"],
+    permLevel: 3
+};
+
+exports.help = {
+    name: `relocatew2`,
+    description: `Gets w2 the fuck out of your voice channel`,
+    usage: `relocatew2`
+};
