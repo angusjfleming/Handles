8f2a47c0cb8277327471c5531c9e65713f3d0aec
Gus <gusmagician@gmail.com>
2017-06-24 13:59:57 +0100

    no longer tries any funny business

--- funcs/logmessage.js
+++ funcs/logmessage.js
@@ -1,6 +1,6 @@
-const PersistentCollection = require("djs-collection-persistent");
-const logs = new PersistentCollection({name: "logs"});
+//const PersistentCollection = require("djs-collection-persistent");
+//const logs = new PersistentCollection({name: "logs"});
 
 module.exports = (bot, msg) => {
-    logs.set(msg.id, {"content":msg.content})
+    //logs.set(msg.id, {"content":msg.content})
 }
