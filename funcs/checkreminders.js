008d3e1e6181765a8ea73a3d8f473abba124e07d
Gus <gusmagician@gmail.com>
2017-06-25 12:41:44 +0100

    Beautified

--- funcs/checkreminders.js
+++ funcs/checkreminders.js
@@ -1,22 +1,22 @@
 module.exports = (bot, fs) => {
-    fs.readdir("./reminders/", (err, files) => {
-        var currentDate = new Date();
-        if (err)
-            console.error(err);
-        files.forEach(f => {
-               let reminder = require(`../reminders/${f}`);
-                if (new Date(reminder.time) < new Date(currentDate)) {
-                    if (reminder.message) {
-                    bot.users.find("id", reminder.authorid).send(`reminding you of \`${reminder.message}\`.`).then(m => {
+  fs.readdir("./reminders/", (err, files) => {
+    var currentDate = new Date();
+    if (err)
+      console.error(err);
+    files.forEach(f => {
+      let reminder = require(`../reminders/${f}`);
+      if (new Date(reminder.time) < new Date(currentDate)) {
+        if (reminder.message) {
+          bot.users.find("id", reminder.authorid).send(`reminding you of \`${reminder.message}\`.`).then(m => {
             setTimeout(m.delete.bind(m), 30000)
-        })
-                    } else {
-                    bot.users.find("id", reminder.authorid).send(`reminding you.`).then(m => {
+          })
+        } else {
+          bot.users.find("id", reminder.authorid).send(`reminding you.`).then(m => {
             setTimeout(m.delete.bind(m), 30000)
-        })
-                    }
-                    fs.unlink(`./reminders/${f}`,function(err){})
-                } 
-        });
+          })
+        }
+        fs.unlink(`./reminders/${f}`, function(err) {})
+      }
     });
+  });
 };
