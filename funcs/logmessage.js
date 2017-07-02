cb8c725b3d76478f26b1ce7ea1314ad710094f12
Gus <gusmagician@gmail.com>
2017-07-02 17:24:05 +0100

    Removed logmessage.js

--- funcs/logmessage.js
+++
@@ -1,6 +0,0 @@
-//const PersistentCollection = require("djs-collection-persistent");
-//const logs = new PersistentCollection({name: "logs"});
-
-module.exports = (bot, msg) => {
-    //logs.set(msg.id, {"content":msg.content})
-}
