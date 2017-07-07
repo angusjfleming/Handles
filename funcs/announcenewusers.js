f87e0d76601e1608cb533da6df45f57d83f76b17
Gus <gusmagician@gmail.com>
2017-07-07 17:26:59 +0100

    Removed guildconfig shit

--- funcs/announcenewusers.js
+++
@@ -1,9 +0,0 @@
-module.exports = (bot, guildmember) => {
-    if (typeof bot.guildconfigs[guildmember.guild.id].announcenewusers === undefined) {
-        return;
-    }
-    if (bot.guildconfigs[guildmember.guild.id].announcenewusers == true) {
-        guildmember.guild.defaultChannel.send(`Welcome, <@${guildmember.user.id}> to ${msg.guild.name}!.`)
-    }
-    
-};
