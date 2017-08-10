3452c1a26d81bc622c6016ab87d1484668d9bc0e
Gus <gusmagician@gmail.com>
2017-08-10 14:52:38 +0100

    Used to fetch cardinfo for hscards command

---
+++ cmd/fetchcardinfo.js
@@ -0,0 +1,21 @@
+const hssql = require("hearthstone-sql")
+exports.run = (bot, msg, params = []) => {
+    hssql.fetchdata(bot.mashapekey)
+    .then(() => {
+        hssql.importcards().then(() => {
+        msg.channel.send("Sucessfully retrieved card information.")
+        })
+    })
+};
+
+exports.help = {
+    name: `fetchcardinfo`,
+    description: `fetches card info the hs card command.`,
+    usage: `fetchcardinfo`
+};
+
+exports.conf = {
+    enabled: true,
+    aliases: [],
+    permLevel: 4
+};
