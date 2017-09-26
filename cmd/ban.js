6796581fe480531c62169aab2cb68cdcad409323
Gus <gusmagician@gmail.com>
2017-09-26 18:12:52 +0100

    Added kick, ban

---
+++ cmd/ban.js
@@ -0,0 +1,31 @@
+exports.run = (bot, msg, params = []) => {
+    try {
+        if (msg.mentions.users.has(bot.user)){
+        var checkuser = msg.mentions.users.first(2)[1]
+        } else {
+        var checkuser = msg.mentions.users.first()
+        var guilduser = msg.guild.members.get(checkuser.id)
+        }
+    } catch (err) {
+        msg.channel.send(`You didn't give me a user to ban.`)
+        return;
+    }
+    params.shift()
+    guilduser.ban(params.join(" ")).then(() => {
+        msg.channel.send(`Successfully banned ${checkuser.tag}`)
+    }).catch((err) => {
+        msg.channel.send(`Failed to ban ${checkuser.tag}, make sure their highest role isn't higher than my highest role.`)
+    })
+};
+
+exports.help = {
+    name: `ban`,
+    description: `Bans a guild member.`,
+    usage: `ban <usertag>`
+};
+
+exports.conf = {
+    enabled: true,
+    aliases: [],
+    permLevel: 3
+};
---
+++ cmd/kick.js
@@ -0,0 +1,31 @@
+exports.run = (bot, msg, params = []) => {
+    try {
+        if (msg.mentions.users.has(bot.user)){
+        var checkuser = msg.mentions.users.first(2)[1]
+        } else {
+        var checkuser = msg.mentions.users.first()
+        var guilduser = msg.guild.members.get(checkuser.id)
+        }
+    } catch (err) {
+        msg.channel.send(`You didn't give me a user to kick.`)
+        return;
+    }
+    params.shift()
+    guilduser.kick(params.join(" ")).then(() => {
+        msg.channel.send(`Successfully kicked ${checkuser.tag}`)
+    }).catch((err) => {
+        msg.channel.send(`Failed to kick ${checkuser.tag}, make sure their highest role isn't higher than my highest role.`)
+    })
+};
+
+exports.help = {
+    name: `kick`,
+    description: `Kicks a guild member.`,
+    usage: `kick <usertag>`
+};
+
+exports.conf = {
+    enabled: true,
+    aliases: [],
+    permLevel: 3
+};
