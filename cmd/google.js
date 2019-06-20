1c3dd56e72ad650fe08fb43a1117172c40259ce4
Gus <realXIV@users.noreply.github.com>
2019-06-20 18:51:42 +0100

    Fixed that which were broken love ;)

--- cmd/google.js
+++ cmd/google.js
@@ -1,28 +1,41 @@
-var scraper = require('google-search-scraper');
+var cheerio = require("cheerio");
+const request = require('request');
+var rgx2 = /\&(.*)/;
+var options = {
+    url: 'http://www.google.com/',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16'
+    }
+};
 
 exports.run = (bot, msg, params = []) => {
-    var first = true
+    var results = []
     var searchTerm = params.join(' ');
 
-    var options = {
-        query: searchTerm,
-        limit: "1"
-    };
     msg.channel.send("Searching...").then(msg => {
-        setTimeout(function() {
-            if (first) {
-                response = randomElement(responses.googlenotfound).replace("<<token>>", `${searchTerm}`)
-                return msg.edit(response);
-            }
-        }, 5000);
+        request(options, function() {
+            request(`https://www.google.co.uk/search?q=${searchTerm}`, function(error, response, body) {
+                var $ = cheerio.load(body);
+                $(".jfp3ef").each(function() {
+                    if (this.children[0].attribs.href) {
+                        var link = this.children[0].attribs.href;
+                        console.log(link)
+                        link = link.slice(7)
+                        link = link.replace(rgx2, "");
+                        if (!link.includes("gstatic")) results.push(link);
+                    } else {}
 
-        scraper.search(options, function(err, url) {
-            if (err) throw err;
-            if (first) {
-                first = false
-                response = randomElement(responses.googlefound).replace("<<token>>", `${url}`)
-                return msg.edit(response);
-            }
+                });
+                console.log(results)
+                if (results[0] == undefined) {
+                    response = randomElement(responses.googlenotfound).replace("<<token>>", `${searchTerm}`)
+                    return msg.edit(response);
+                } else {
+                    response = randomElement(responses.googlefound).replace("<<token>>", `${results[0]}`)
+                    return msg.edit(response);
+                }
+            });
+
         });
     });
 };
